52ec21ee89598f13f8e83b43f1d0d4f089955e12
Patrick Pamponet Steiger <psteiger@gmail.com>
2021-08-23 04:06:25 -0300

    Create `lib` module and add jitpack.yml file for Jitpack builds.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.5.21"
     id("java-library")
@@ -9,15 +11,12 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-group = "com.freelapp.tasksflow"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 afterEvaluate {
